a3efffce4333950f330c842f8e1c2d2feec7edd2
nettrac <neettrac8@nettrac.net>
2017-09-29 16:23:25 +0800

    sorry icons

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -39,7 +39,8 @@
             <br/>
             <span class="servicestitle"> FREE WEB</span>
             <br/>
-            <span class="label-important"> FREE</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+FREE</span>
             <p class="definition">
                 5 Pages of content, NetTrac Template based. Fast and Free!
             </p>
@@ -48,7 +49,8 @@
 
             <span class="servicestitle"> STARTER WEB</span>
             <br/>
-            <span class="label-important"> Php 10,000</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 10,000</span>
             <p class="definition">
                 6 Pages of content, ideal to get your corporate identity on the net.
             </p>
@@ -56,14 +58,16 @@
             <br/>
             <span class="servicestitle"> CORPORATE WEB</span>
             <br/>
-            <span class="label-important"> Php 15,000</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 15,000</span>
             <p class="definition">
                 10 Pages of content, with Database for Dynamic contents.
             </p>
             <br/>
             <span class="servicestitle"> ENTERPRISE WEB</span>
             <br/>
-            <span class="label-important"> Php 30,000</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 30,000</span>
             <p class="definition">
                 20 Pages of content, 3 Database and Ecommerce setup.
             </p>
@@ -74,7 +78,8 @@
             <br/>
             <span class="servicestitle"> STARTER LINUX</span>
             <br/>
-            <span class="label-important"> Php 149/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 149/month</span>
             <p class="definition">
                 500 MB Storage, 5 POP3 Email Accounts 5GB transfer per month PHP4/5 and MYSQL for 3 Databases, Unlimited Email Forwarding.
             </p>
@@ -82,7 +87,8 @@
 
             <span class="servicestitle"> BASIC LINUX</span>
             <br/>
-            <span class="label-important"> Php 229/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 229/month</span>
             <p class="definition">
                 1 GB Storage, 30 POP3 Email Accounts. 25GB transfer Bandwidth per month. PHP4/5 MYSQL for up to 6 Databases. Unlimited Email Forwarding. Host 2 domains.
             </p>
@@ -90,7 +96,8 @@
 
             <span class="servicestitle"> BUSINESS LINUX</span>
             <br/>
-            <span class="label-important"> Php 299/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 299/month</span>
             <p class="definition">
                 2 GB Storage, 100 POP3 Email Accounts. 25GB transfer Bandwidth per month. PHP4/5 MYSQL for up to 6 Databases. Unlimited Email Forwarding. Host 3 domains.
             </p>
@@ -101,7 +108,8 @@
             <br/>
             <span class="servicestitle"> BE 1000</span>
             <br/>
-            <span class="label-important"> Php 400/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 400/month</span>
             <p class="definition">
                 Sends 1000 emails per month from your control panel.
             </p>
@@ -109,7 +117,8 @@
 
             <span class="servicestitle"> BE 2500</span>
             <br/>
-            <span class="label-important"> Php 800/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 800/month</span>
             <p class="definition">
                 Sends 2500 emails per month from your control panel.
             </p>
@@ -117,7 +126,8 @@
 
             <span class="servicestitle"> BE 3500</span>
             <br/>
-            <span class="label-important"> Php 1,000/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 1,000/month</span>
             <p class="definition">
                 Sends 3500 emails per month from your control panel.
             </p>
@@ -125,7 +135,8 @@
 
             <span class="servicestitle"> BE 10000</span>
             <br/>
-            <span class="label-important"> Php 2,400/month</span>
+            <span class="label-important"> <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+Php 2,400/month</span>
             <p class="definition">
                 Sends 10000 emails per month from your control panel.
             </p>
