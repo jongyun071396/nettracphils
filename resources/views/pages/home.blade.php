68911441b9d2038e04f0baa757e9f963633065c6
nettrac <neettrac8@nettrac.net>
2017-09-29 15:52:32 +0800

    home content and support

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,16 +1,135 @@
-@extends('layouts.app')
-
-@section('custom-css')
+@extends('layouts.app') @section('custom-css')
 <style>
-	nav.navbar {
-		background: rgba(0, 0, 0, 0.3);
-	}
+    nav.navbar {
+        background: rgba(0, 0, 0, 0.3);
+    }
 </style>
-@endsection
 
-@section('body')
-	{{-- hero --}}
-	<div id="hero"></div>
+@endsection @section('body') {{-- hero --}}
+<div id="hero">
+    <div class="container hero-content">
+        <br>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                  UNLIMITED HOSTING OPTIONS
+                </div>
+                <div class="item">
+                    GET ALL THE BUSINESS TOOLS<br/> YOU NEED
+                </div>
+
+                <div class="item">
+                   NEED A WEBSITE?<br/>
+                   NetTrac is your Lowest Cost Solutions
+                </div>
+            </div>
+
+            <!-- Left and right controls -->
+     
+        </div>
+    </div>
+</div>
+<div class="container" id="home_content">
+    <div class="row justify-content-md-center" id="types" style="margin-left:70px;">
+        <div class="col-md-3 services">
+            <i class="fa fa-desktop fa-5x" aria-hidden="true" style="padding-left:35%;color:#F2635F"></i>
+            <p class="title" style="text-align: center;">DEVELOPMENT </p>
+            <br/>
+            <span class="servicestitle"> FREE WEB</span>
+            <br/>
+            <span class="label-important"> FREE</span>
+            <p class="definition">
+                5 Pages of content, NetTrac Template based. Fast and Free!
+            </p>
+
+            <br/>
+
+            <span class="servicestitle"> STARTER WEB</span>
+            <br/>
+            <span class="label-important"> Php 10,000</span>
+            <p class="definition">
+                6 Pages of content, ideal to get your corporate identity on the net.
+            </p>
+
+            <br/>
+            <span class="servicestitle"> CORPORATE WEB</span>
+            <br/>
+            <span class="label-important"> Php 15,000</span>
+            <p class="definition">
+                10 Pages of content, with Database for Dynamic contents.
+            </p>
+            <br/>
+            <span class="servicestitle"> ENTERPRISE WEB</span>
+            <br/>
+            <span class="label-important"> Php 30,000</span>
+            <p class="definition">
+                20 Pages of content, 3 Database and Ecommerce setup.
+            </p>
+        </div>
+        <div class="col-md-3 col-md-offset-1 services">
+            <i class="fa fa-tasks fa-5x" aria-hidden="true" style="padding-left:36%;color:#F2635F"></i>
+            <p class="title" style="text-align: center;">HOSTING</p>
+            <br/>
+            <span class="servicestitle"> STARTER LINUX</span>
+            <br/>
+            <span class="label-important"> Php 149/month</span>
+            <p class="definition">
+                500 MB Storage, 5 POP3 Email Accounts 5GB transfer per month PHP4/5 and MYSQL for 3 Databases, Unlimited Email Forwarding.
+            </p>
+            <br/>
+
+            <span class="servicestitle"> BASIC LINUX</span>
+            <br/>
+            <span class="label-important"> Php 229/month</span>
+            <p class="definition">
+                1 GB Storage, 30 POP3 Email Accounts. 25GB transfer Bandwidth per month. PHP4/5 MYSQL for up to 6 Databases. Unlimited Email Forwarding. Host 2 domains.
+            </p>
+            <br/>
+
+            <span class="servicestitle"> BUSINESS LINUX</span>
+            <br/>
+            <span class="label-important"> Php 299/month</span>
+            <p class="definition">
+                2 GB Storage, 100 POP3 Email Accounts. 25GB transfer Bandwidth per month. PHP4/5 MYSQL for up to 6 Databases. Unlimited Email Forwarding. Host 3 domains.
+            </p>
+        </div>
+        <div class="col-md-3 col-md-offset-1 services">
+            <i class="fa fa-envelope fa-5x" aria-hidden="true" style="padding-left: 35%;color:#F2635F"></i>
+            <p class="title" style="text-align: center;">BULK MAIL</p>
+            <br/>
+            <span class="servicestitle"> BE 1000</span>
+            <br/>
+            <span class="label-important"> Php 400/month</span>
+            <p class="definition">
+                Sends 1000 emails per month from your control panel.
+            </p>
+            <br/>
+
+            <span class="servicestitle"> BE 2500</span>
+            <br/>
+            <span class="label-important"> Php 800/month</span>
+            <p class="definition">
+                Sends 2500 emails per month from your control panel.
+            </p>
+            <br/>
+
+            <span class="servicestitle"> BE 3500</span>
+            <br/>
+            <span class="label-important"> Php 1,000/month</span>
+            <p class="definition">
+                Sends 3500 emails per month from your control panel.
+            </p>
+            <br/>
+
+            <span class="servicestitle"> BE 10000</span>
+            <br/>
+            <span class="label-important"> Php 2,400/month</span>
+            <p class="definition">
+                Sends 10000 emails per month from your control panel.
+            </p>
+        </div>
+    </div>
+</div>
 @stop
-
-
--- resources/views/pages/support.blade.php
+++ resources/views/pages/support.blade.php
@@ -1,4 +1,37 @@
 @extends('layouts.app')
 @section('body')
-	
+	<div class="container" style="padding:30px">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>SUPPORT</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-5">
+				<h5>To Reach a Support Technician Please Call any of the following Phone Numbers During the office hours indicated: </h5>
+			</div>
+		</div>
+		<div class="row" style="padding-top:40px">
+			<div class="col-md-6">
+				<b>Support Desk Hours</b><br/>
+				For S.E. Asia Customers in GMT +8 Live support is available:<br/>
+				8:00AM to 4:30PM and<br/>
+				7:00PM to 7:00AM 
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6" style="padding-top:40px">
+				 <b>Support Desk Phone Numbers:<br/>
+
+				S.E. Asia Customers Call: +63 032-268-2565<br/>
+
+				Australlian Customers Call: +61 2 6181-0700<br/>
+
+				Toll Free in North America: 1-877-625-5444 and Select Option 1<br/>
+
+				Live support is also available via CHAT on our website during these times.<br/>
+				</b>
+			</div>
+		</div>
+	</div>
 @endsection
