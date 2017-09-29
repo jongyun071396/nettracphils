2e20e110f9857ade338a582a1654ceafa03ff4e8
staff12@nettrac.net <staff12@nettrac.net>
2017-09-29 14:00:32 +0800

    employment and support

--- resources/views/pages/employment.blade.php
+++ resources/views/pages/employment.blade.php
@@ -3,106 +3,110 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				
 				<h2>NetTrac Communications Inc. - Job Opportunities</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
 				<h4>Current NetTrac Opportunities: 	Website Application Designer/Graphic Artist/ required for Immediate Hiring.</h4>
-				<div class="col-xs-6">
-					<p>
+			</div>
+			<div class="col-md-6">
+				<p>
+					<b>Requirements:</b> 
+					<ul>
+						<li>
+							Skilled in Adobe Photoshop and Flash CS4. 
+						</li>
+						<li>
+							Ability to translate business and marketing requirements into presentable web and media designs.
+						</li>
+						<li>
+							Fluent in spoken and written English. 
+						</li>
+						<li>
+							Viewable Portfolio of completed designs, demonstrating your skill level in Flash and static designs. 
+						</li>
+						<li>
+							Understands CSS and be able to convert designs to html. 
+						</li>
+						<li>
+							JavaScript experience. 
+						</li>
+						<li>
+							References. 
+						</li>
+						<li>
+							University Degree in related field. 
+						</li>
+						<li>
+							NBI Clearance. SSS# . PhilHealth# 
+						</li>
+						<li>
+							Submit the following with your application: Brief description of your experience in PHP development.
+						</li>
+						<li>
+							 Sample code from a project you have worked on. 
+						</li>
+					</ul>
+					Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span> 
+					<br>
+				</p>
+				<p>
+					<b>Commission Based Sales Representative (Ontario):</b> <br>	
+					NetTrac Communications Inc. is seeking a dynamic Sales/Marketing person. You will benefit from our existing client base, and have access to unlimited sales and technical support from our highly experienced team. <br>
+					The candidate will need to demonstrate a high level of independence and be a self starter. All aspects of Direct Marketing will be involved in this position and is an excellent home office opportunity yielding higher than average commissions.  <br>
+					Better than average communication skills and demonstrated industry product knowledge is a must. The candidate requires reliable transportation. <br>
+					<b>Remuneration:</b>Flat "across the board" commission (above industry average) with residuals payable on recurring contracts. <br><br>
+					Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span>
+				</p>
+			</div>
+			<div class="col-xs-6">
+				<p>
+					<b>PHP Web Application Developers (South East Asia Office):</b><br>
+					Due to increasing work volumes, we are looking for a team member to join our custom development group. In our Cebu office, you will be working with staff in Canada to meet customer needs. 
+					<b>Remuneration:</b> Based on experience and skill level. <br>
 					<b>Requirements:</b> 
 						<ul>
 							<li>
-								Skilled in Adobe Photoshop and Flash CS4. 
+								A strong background in PHP programming demonstrating experience with up to date application models. 
 							</li>
 							<li>
-								Ability to translate business and marketing requirements into presentable web and media designs.
+								A solid background in Database design. 
 							</li>
 							<li>
-								Fluent in spoken and written English. 
+								Be comfortable with code versioning such as SVN or CVS. 
 							</li>
 							<li>
-								Viewable Portfolio of completed designs, demonstrating your skill level in Flash and static designs. 
+								Have a workable knowledge of Linux environments and be comfortable working with shell access.
 							</li>
 							<li>
-								Understands CSS and be able to convert designs to html. 
-							</li>
-							<li>
-								JavaScript experience. 
-							</li>
-							<li>
-								References. 
-							</li>
-							<li>
-								University Degree in related field. 
-							</li>
-							<li>
-								NBI Clearance. SSS# . PhilHealth# 
+								Possess excellent verbal and written communication skills in English. 
 							</li>
 							<li>
 								Submit the following with your application: Brief description of your experience in PHP development.
 							</li>
 							<li>
-								 Sample code from a project you have worked on. 
+								Sample code from a project you have worked on.
 							</li>
 						</ul>
-						Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span> 
-						<br>
-					</p>
-					<p>
-						<b>Commission Based Sales Representative (Ontario):</b> <br>	
-						NetTrac Communications Inc. is seeking a dynamic Sales/Marketing person. You will benefit from our existing client base, and have access to unlimited sales and technical support from our highly experienced team. <br>
-						The candidate will need to demonstrate a high level of independence and be a self starter. All aspects of Direct Marketing will be involved in this position and is an excellent home office opportunity yielding higher than average commissions.  <br>
-						Better than average communication skills and demonstrated industry product knowledge is a must. The candidate requires reliable transportation. <br>
-						<b>Remuneration:</b>Flat "across the board" commission (above industry average) with residuals payable on recurring contracts. <br><br>
-						Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span>
-					</p>
-				</div>
-				<div class="col-xs-6">
-					<p>
-						<b>PHP Web Application Developers (South East Asia Office):</b><br>
-						Due to increasing work volumes, we are looking for a team member to join our custom development group. In our Cebu office, you will be working with staff in Canada to meet customer needs. 
-						<b>Remuneration:</b> Based on experience and skill level. <br>
-						<b>Requirements:</b> 
-							<ul>
-								<li>
-									A strong background in PHP programming demonstrating experience with up to date application models. 
-								</li>
-								<li>
-									A solid background in Database design. 
-								</li>
-								<li>
-									Be comfortable with code versioning such as SVN or CVS. 
-								</li>
-								<li>
-									Have a workable knowledge of Linux environments and be comfortable working with shell access.
-								</li>
-								<li>
-									Possess excellent verbal and written communication skills in English. 
-								</li>
-								<li>
-									Submit the following with your application: Brief description of your experience in PHP development.
-								</li>
-								<li>
-									Sample code from a project you have worked on.
-								</li>
-							</ul>
-						Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span>
-					</p>
-					<p>
-						<b>Asterisk Expert (5 Years Experience) Remote Access Contract Position </b><br> 
-						<b>Remuneration: Negotiable</b><br>
-						<b>Requirements:</b> 
-							<ul>
-								<li>
-									5 years experience working with Asterisk. Able to work remotely. TASK: to assist on an 'as needed basis' to make changes and upgrades to our Asterisk system in Canada. 
-								</li>
-								<li>								
-									Submit in writing your Asterisk experience and capabilities with specific references. 
-								</li>
-							</ul>
-						Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span>
-					</p>
-				</div>
+					Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span>
+				</p>
+				<p>
+					<b>Asterisk Expert (5 Years Experience) Remote Access Contract Position </b><br> 
+					<b>Remuneration: Negotiable</b><br>
+					<b>Requirements:</b> 
+						<ul>
+							<li>
+								5 years experience working with Asterisk. Able to work remotely. TASK: to assist on an 'as needed basis' to make changes and upgrades to our Asterisk system in Canada. 
+							</li>
+							<li>								
+								Submit in writing your Asterisk experience and capabilities with specific references. 
+							</li>
+						</ul>
+					Please respond by emailing your resume in MSWORD.DOC format only to: <span class="email">employment1@nettrac.net </span>
+				</p>
 			</div>
 		</div>
 	</div>
+</div>
 @endsection
--- resources/views/pages/support.blade.php
+++ resources/views/pages/support.blade.php
@@ -1,4 +1,47 @@
 @extends('layouts.app')
+@section('footer')
+	<style>
+		footer {
+		    position: fixed;
+		    height: 100px;
+		    bottom: 0;
+		    width: 100%;
+		}
+	</style>
+@endsection
 @section('body')
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h2>NetTrac Communications Inc. - Support</h2><br>
+				<ul style="list-style: none">
+					<li>
+						<b>To Open a Support Ticket Please Use This Link (You must have a NetTrac Control Panel Login to Access this Portal):</b> 
+						<span class="email">supportcenter.nettrac.net</span> 
+						<br>
+						<b>To Reach a Support Technician Please Call any of the following Phone Numbers During the office hours indicated: </b>
+						<br><br> 
+						<b>Support Desk Hours</b><br>
+						For S.E. Asia Customers in GMT +8 Live support is available:<br>
+
+						8:00AM to 4:30PM and 7:00PM to 7:00AM <br><br>
+						<b>
+						Support Desk Phone Numbers: <br>	
+
+						S.E. Asia Customers Call: +63 032-268-2565 <br>
+
+						Australlian Customers Call: +61 2 6181-0700 <br>
+
+						Toll Free in North America: 1-877-625-5444 and Select Option 1 <br>
+
+						Live support is also available via CHAT on our website during these times.</b><br> <br>
+
+						It is no longer possible to email support directly. To contact support staff you can call directly for support or open a support ticket. <br>
+						This change has been made to streamline our support services. 
+					</li>
+				</ul>
+				
+			</div>
+		</div>
+	</div>
 @endsection
